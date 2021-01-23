b5a07d33d698c9119807ae24a05cd01fe828163e
Dhruv Jawalkar <jawalkad@oregonstate.edu>
2021-01-23 14:25:54 -0800

    Time: 0 ms (100.00%), Space: 41.4 MB (65.98%) - LeetHub

---
+++ count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+  // Return tree depth in O(d) time.
+  public int computeDepth(TreeNode node) {
+    int d = 0;
+    while (node.left != null) {
+      node = node.left;
+      ++d;
+    }
+    return d;
+  }
+​
+  // Last level nodes are enumerated from 0 to 2**d - 1 (left -> right).
+  // Return True if last level node idx exists. 
+  // Binary search with O(d) complexity.
+  public boolean exists(int idx, int d, TreeNode node) {
+    int left = 0, right = (int)Math.pow(2, d) - 1;
+    int pivot;
+    for(int i = 0; i < d; ++i) {
+      pivot = left + (right - left) / 2;
+      if (idx <= pivot) {
+        node = node.left;
+        right = pivot;
+      }
+      else {
+        node = node.right;
+        left = pivot + 1;
+      }
+    }
+    return node != null;
+  }
