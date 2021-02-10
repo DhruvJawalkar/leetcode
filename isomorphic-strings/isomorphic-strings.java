f6ccfc862b6679c458b6d0285659f0b27889f068
Dhruv Jawalkar <jawalkad@oregonstate.edu>
2021-02-10 13:18:28 -0800

    Time: 4 ms (89.88%), Space: 39.2 MB (38.99%) - LeetHub

--- isomorphic-strings/isomorphic-strings.java
+++ isomorphic-strings/isomorphic-strings.java
@@ -1,38 +1,13 @@
 class Solution {
-    public boolean isIsomorphic(String s, String t) {
-        //base cases
-        if(s==null || t==null)
-            return s.equals(t);
-        
-        if(s.length()!=t.length())
-            return false;
-        
-        //initialize hashmap
-        HashMap<Character, Character> aToB = new HashMap<>();
-        HashMap<Character, Character> bToA = new HashMap<>();
-        
-        //iterate over strings
-        //check if mapping exists, if yes then verify, else put
+public boolean isIsomorphic (String s, String t) {
+        int[] m1 = new int[256];
+        int[] m2 = new int[256];
         int n = s.length();
-        for(int i=0; i<n; i++){
-            char chA = s.charAt(i);
-            char chB = t.charAt(i);
-            
-            if(!aToB.containsKey(chA)){
-                if(!bToA.containsKey(chB)){
-                    aToB.put(chA, chB);
-                    bToA.put(chB, chA);
-                }
-                else 
-                    return false;
-            }
-            else if(aToB.get(chA)!=chB)
-                return false;
+        for (int i = 0; i < n; i++) {
+            if (m1[s.charAt(i)] != m2[t.charAt(i)]) return false;
+            m1[s.charAt(i)] = i + 1;
+            m2[t.charAt(i)] = i + 1;
         }
-        
-
-        
-        //return true default
         return true;
     }
 }
