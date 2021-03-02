f6eb480a5bd9bc09c77c05dd3c37c7d5e8af1d5c
Dhruv Jawalkar <jawalkad@oregonstate.edu>
2021-03-02 14:33:28 -0800

    Time: 0 ms (100.00%), Space: 36.9 MB (76.02%) - LeetHub

--- robot-bounded-in-circle/robot-bounded-in-circle.java
+++ robot-bounded-in-circle/robot-bounded-in-circle.java
@@ -6,43 +6,30 @@ class Solution {
         if(instructions==null || instructions.length()==0)
             return true;
         
-        //intialise robot dir, N->0, E->1, S->2, W->3 and postion to [0,0]
+        //intialise robot dir, N->0, E->1, S->2, W->3 and postion x=0, y=0
         int robotDir = 0;
-        int[] robotPos = new int[]{0,0};
+        int x=0, y=0;
+        int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
         
         //iterate over instruction length and update robot dir
-        for(int i=0; i<instructions.length(); i++){
-            char ch = instructions.charAt(i);
-            
-            if(ch=='L'){
+        for(char curIns : instructions.toCharArray()){
+            if(curIns=='L'){
                 robotDir +=3;
                 robotDir=robotDir%4;
             }
-            else if(ch=='R'){
-                robotDir +=1;
+            else if(curIns=='R'){
+                robotDir+=1;
                 robotDir=robotDir%4;
             }
             else{
-                switch(robotDir){
-                    case 0:
-                        robotPos[1] += 1; 
-                        break;    
-                    case 1:
-                        robotPos[0] += 1; 
-                        break;
-                    case 2:
-                        robotPos[1] -= 1; 
-                        break;    
-                    case 3:
-                        robotPos[0] -= 1; 
-                        break;    
-                }
+                x+=dirs[robotDir][0];
+                y+=dirs[robotDir][1];
             }
         }
         
         //in the end check if robot is facing the same dir as initial, N=0 and not at (0,0)
         //return false
-        if(robotDir==0 && (robotPos[0]!=0 || robotPos[1]!=0))
+        if(robotDir==0 && (x!=0 || y!=0))
             return false;
         
         //else return true
