c470d665f45e789d3c894a1aa57722eadc6cd052
Dhruv Jawalkar <jawalkad@oregonstate.edu>
2021-03-17 13:18:04 -0700

    Time: 6 ms (76.73%), Space: 39 MB (57.11%) - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.java
+++ meeting-rooms-ii/meeting-rooms-ii.java
@@ -1,18 +1,28 @@
+//TC - O(n log n)
+//SC - O(n)
+
+//Ex: [[7,10],[2,4]]
+
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        if(intervals==null || intervals.length==0) return 0;
+        //base cases
+        if(intervals==null || intervals.length==0 || intervals[0].length==0)
+            return 0;
         
-        Arrays.sort(intervals, (int[] m1, int[] m2) -> m1[0] - m2[0]);
+        //sort intervals acc to start time
+        Arrays.sort(intervals, (m1, m2) -> m1[0] - m2[0]);
         
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        //init a min heap which compares end times for meetings
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        for(int[] m : intervals){
-            if(pq.size()>0 && pq.peek()<=m[0]){
+        for(int[] meeting : intervals){
+            if(!pq.isEmpty() && pq.peek()<=meeting[0])
                 pq.poll();
-            }
-            pq.add(m[1]);
+            
+            pq.add(meeting[1]);
         }
-        
-        return pq.size();
+                
+        //return heap size as minRoomsRequired
+        return pq.size();   
     }
 }
