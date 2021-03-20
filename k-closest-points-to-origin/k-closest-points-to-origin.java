4b39772981aee00bb11373d91522ff58b250ecc9
Dhruv Jawalkar <jawalkad@oregonstate.edu>
2021-03-19 18:30:49 -0700

    Time: 28 ms (46.23%), Space: 47.4 MB (81.14%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.java
+++ k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,38 +1,28 @@
-//TC - O(n log k)
-//SC - O(k)
-//points = [[1,3],[-2,2]], k = 1
-
 class Solution {
-    public int[][] kClosest(int[][] points, int K) {        
-        //init max heap with k elements, output
-        PriorityQueue<int[]> pq = new PriorityQueue<>(K, (p1, p2) -> p2[2] - p1[2]);        
-        int[][] output = new int[K][2];
+    public int[][] kClosest(int[][] points, int K) {
         
-        //iterate over points
-        for(int i=0; i<points.length; i++){
-            int distance = (int)Math.pow(points[i][0], 2) + (int)Math.pow(points[i][1], 2); 
-            
-                if(pq.isEmpty() || pq.size()<K)
-                    pq.add(new int[]{points[i][0], points[i][1], distance});    
-                else{
-                    if(distance < pq.peek()[2]){
-                        pq.poll();
-                        pq.add(new int[]{points[i][0], points[i][1], distance});
-                    }                 
-                }
-        }
-
-        //pop k times to get k closest points, put popped element[0], element[1]  
-        int i = K-1;
-        while(!pq.isEmpty()){
-            int[] point = pq.poll();
-            output[i][0] = point[0];
-            output[i][1] = point[1];
-            i--;
+        PriorityQueue<int[]> heap = new PriorityQueue<int[]>(new Comparator<int[]>() { 
+            @Override
+            public int compare(int[] left, int[] right) {
+                return getDistance(right) - getDistance(left);
+            }
+        });
+        
+        for (int[] point: points) {
+            heap.add(point);
+            if (heap.size() > K)
+                heap.poll();
         }
         
-        //return output
-        return output;
+        int[][] result = new int[K][2];
+        while (K > 0)
+            result[--K] = heap.poll();
+        
+        return result;
+         
     }
-
+    
+    private int getDistance(int [] point) {
+        return point[0] * point[0] + point[1] * point[1];
+    }
 }
