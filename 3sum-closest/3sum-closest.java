e455a2ed1e2d6ec027e88d6adb5e808143a00bbd
Dhruv Jawalkar <jawalkad@oregonstate.edu>
2021-03-24 15:59:44 -0700

    Time: 4 ms (85.76%), Space: 38.7 MB (44.50%) - LeetHub

--- 3sum-closest/3sum-closest.java
+++ 3sum-closest/3sum-closest.java
@@ -1,39 +1,56 @@
-//Ex - nums = [-1,2,1,-4], target = 1
-//TC - O(n^2)
-//SC - O(log n)
+/*
+TC - O(n^2)
+SC - O(1)
 
+Ex: [-3,-2,-5,3,-4]
+*/
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
+        //base case
+        if(nums==null || nums.length<3)
+            return -1;
+        
         //init closestSum
-        int n = nums.length;
-        int closestSum = nums[0] + nums[1] + nums[n-1];
+        int closestSum = nums[0]+nums[1]+nums[nums.length-1];
         
         //sort nums
         Arrays.sort(nums);
         
-        int start, end, curSum;
-        
-        //iterate over nums, move i from 0 till n-3
-        for(int i=0; i<n-2; i++){
-            start = i+1;
-            end = n-1;
+        //iterate over nums, init low and high
+            //move pointers according to curSum
+        //[-5,-4,-3,-2,3]
+        int low = 1, high = nums.length-1, curSum = 0;
+        for(int i=0; i<nums.length; i++){
+            if(i!=0 && nums[i]==nums[i-1])
+                continue;
             
-            while(start<end){ 
-                curSum = nums[i] + nums[start] + nums[end];
+            low = i+1;
+            high = nums.length-1;
+            
+            while(low < high){
+                curSum = nums[i] + nums[low] + nums[high];
                 
-                if(Math.abs(curSum-target)<Math.abs(closestSum-target))
-                    closestSum = curSum;        
-        
                 if(curSum==target)
                     return curSum;
                 
-                else if(curSum<target)
-                    start++;
-                else
-                    end--;                
+                if(curSum<target){
+                    low++;
+                    
+                    while(low<high && nums[low]==nums[low-1])
+                        low++;
+                }
+                else{
+                    high--;
+                    
+                    while(low<high && nums[high]==nums[high+1])
+                        high--;
+                } 
+                
+                if(Math.abs(curSum-target) < Math.abs(closestSum-target))
+                    closestSum = curSum;
             }
         }
-   
+        
         //return closestSum
         return closestSum;
     }
