c41637a2033c8849b2206d97fb06cbbdde4e5c4c
Dhruv Jawalkar <jawalkad@oregonstate.edu>
2021-04-16 13:45:38 -0700

    Time: 44 ms (77.85%), Space: 43.8 MB (21.98%) - LeetHub

---
+++ moving-average-from-data-stream/moving-average-from-data-stream.java
@@ -0,0 +1,25 @@
+class MovingAverage {
+  int size, head = 0, windowSum = 0, count = 0;
+  int[] queue;
+  public MovingAverage(int size) {
+    this.size = size;
+    queue = new int[size];
+  }
+
+  public double next(int val) {
+    ++count;
+    // calculate the new sum by shifting the window
+    int tail = (head + 1) % size;
+    windowSum = windowSum - queue[tail] + val;
+    // move on to the next head
+    head = (head + 1) % size;
+    queue[head] = val;
+    return windowSum * 1.0 / Math.min(size, count);
+  }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.next(val);
+ */
