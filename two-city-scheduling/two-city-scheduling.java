0c29691091bdeb9a32e9b547b94d86db57dbce70
Dhruv Jawalkar <jawalkad@oregonstate.edu>
2021-04-16 12:20:31 -0700

    Time: 0 ms (100.00%), Space: 36.9 MB (66.90%) - LeetHub

--- two-city-scheduling/two-city-scheduling.java
+++ two-city-scheduling/two-city-scheduling.java
@@ -1,18 +1,21 @@
 class Solution {
     public int twoCitySchedCost(int[][] costs) {
-        int N = costs.length / 2;
-        int[][] dp = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            dp[i][0] = dp[i - 1][0] + costs[i - 1][0];
+        
+        int n = costs.length;
+        int[] refund = new int[n];
+        int minCost = 0;
+        
+        for(int i=0; i<costs.length; i++){
+            refund[i] = costs[i][1] - costs[i][0];
+            minCost += costs[i][0];
         }
-        for (int j = 1; j <= N; j++) {
-            dp[0][j] = dp[0][j - 1] + costs[j - 1][1];
+        
+        Arrays.sort(refund);
+        
+        for(int i=0; i<n/2; i++){
+            minCost += refund[i];
         }
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                dp[i][j] = Math.min(dp[i - 1][j] + costs[i + j - 1][0], dp[i][j - 1] + costs[i + j - 1][1]);
-            }
-        }
-        return dp[N][N];
+        
+        return minCost;
     }
 }
