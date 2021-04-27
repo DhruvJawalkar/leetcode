75c1c1febc0d2274798d627de6f5f1d51b2b908e
Dhruv Jawalkar <jawalkad@oregonstate.edu>
2021-04-26 20:40:38 -0700

    Time: 0 ms (100.00%), Space: 38.9 MB (73.27%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,20 +1,14 @@
-/*
-Input: nums = [2,7,11,15], target = 9
-numLookupMap = {
-    2: 0
-}
+//nums = [2,7,11,15], target = 9
+//
 
-TC - O(n)
-SC - O(n)
-*/
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> numLookupMap = new HashMap<>();
         
         for(int i=0; i<nums.length; i++){
-            if(numLookupMap.containsKey(target-nums[i]))
-                return new int[]{numLookupMap.get(target-nums[i]), i};
-            
+            int compliment = target - nums[i];
+            if(numLookupMap.containsKey(compliment))
+                return new int[]{numLookupMap.get(compliment), i};
             else
                 numLookupMap.put(nums[i], i);
         }
